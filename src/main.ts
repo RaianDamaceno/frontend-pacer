fab1de9cfec277d3725411b1ed75a1b5841392fc
Raian Damaceno <36999338+RaianDamaceno@users.noreply.github.com>
2021-09-20 19:18:48 -0300

    Revert "Revert "KON-24 | Criação da tela de adicionar avaliação do usuario""

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import axios from 'axios';
 
 Vue.config.productionTip = false
 
