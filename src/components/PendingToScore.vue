50e64cf41b7455af24f6ebc622bde34405dea6db
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2021-10-10 18:13:44 -0300

    Pendências do Avaliador
    Pendências por id do Avaliador (User).
    - O avaliador está mockado para código 12, de acordo com o DML.
    O restante dos dados está vindo diretamente do BD.

--- src/components/PendingToScore.vue
+++ src/components/PendingToScore.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="dialog" persistent max-width="700px">
+    <v-dialog v-model="dialog" scrollable max-width="700px">
       <template v-slot:activator="{ on, attrs }">
         <v-btn color="primary" dark v-bind="attrs" v-on="on">
           Falta Pontuar
@@ -19,35 +19,50 @@
 
         <v-card-text>
           <v-container>
-            <!-- <v-form ref="form">
-              <v-row>
-                <v-col cols="12">
-                  <v-autocomplete
-                    :items="this.projects"
-                    label="Grupos* "
-                    required
-                    id="project"
-                    item-text="description"
-                    item-value="idProject"
-                    v-model="formProject"
-                  ></v-autocomplete>
-                </v-col>
-              </v-row>
-            </v-form> -->
-            <table>
+            <table
+              v-if="this.erro"
+              border="1px"
+              width="500"
+              style="color:white; background-color:red"
+            >
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>{{ this.erro }}</strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table
+              v-if="this.msg"
+              border="1px"
+              width="500"
+              style="color:white; background-color:green"
+            >
+              <tbody>
+                <tr>
+                  <td>
+                    <strong>{{ this.msg }}</strong>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table v-if="this.listaResumida.length > 0">
               <thead>
                 <!-- <th width="050">CÃ³d</th> -->
-                <th width="280">Projeto/Sprint</th>
-                <th width="280">Avaliado</th>
-                <th width="100"></th>
+                <th width="200px">Projeto/Sprint</th>
+                <th width="30px"></th>
+                <th width="390px">Avaliado</th>
+                <th width="100px%"></th>
               </thead>
               <tbody>
-                <tr v-for="prop in proprietarios" :key="prop.id">
-                  <td width="150">##NomeDoProjeto / NomeDaSprint##</td>  
-                  <td width="300">##Nome do Avaliado##</td>
+                <tr v-for="pend in listaResumida" :key="pend.id">
+                  <td>{{ maskData(pend.initialDate) }} ~ {{maskData(pend.finalDate) }}</td>
+                  <td></td>
+                  <td>{{pend.name}}</td>
                   <td class="pontuar-links">
-                    <button @click="editar(prop)" class="btn_editar">
-                      <i>pontuar</i>
+                    <button @click="btnPontuar(pend.completo)">
+                      <i> pontuar </i>
                     </button>
                   </td>
                 </tr>
@@ -72,28 +87,79 @@ import axios from "axios";
 export default {
   data: () => ({
     dialog: false,
-    projects: [],
-    criterias: [],
-    formProject: "",
-    formCriteria: "",
-    formNotaMinima: "",
-    formNotaMaxima: "",
-    formPesoNota: "",
-    proprietarios: [1,2,3,4,5,6]
+    listaResumida: [],
+    idEvaluator: 12, //TODO
+    erro: null,
+    msg: null,
   }),
-
   methods: {
     async getPendencias() {
-      await axios.get("project").then(
+      this.mostraErro();
+      this.mostraMsg();
+      await axios.get("notes-store/pending/" + this.idEvaluator).then(
         (response) => {
-          this.projects = response.data;
-          console.log(response.data);
+          this.pendencias = response.data;
+          if (this.pendencias.length > 0) {
+            
+            this.listaResumida = [];
+            
+            let aluno;
+
+            this.pendencias.forEach(element => {
+
+              aluno = {};
+              aluno.name = element.evaluated.name;
+              aluno.initialDate = element.sprint.initialDate;
+              aluno.finalDate = element.sprint.finalDate;
+              aluno.completo = element;
+
+              if(this.listaResumida.findIndex( array => array.name === aluno.name ) == -1){
+                this.listaResumida.push(aluno);
+              }
+
+            });
+            
+          } else {
+            this.mostraMsg("Você não tem pendências neste momento!");
+          }
+          //console.log(response.data);
         },
         (error) => {
-          console.log(error);
+          //console.log(error);
+          this.mostraErro("Erro!",10);
         }
       );
-      console.log(this.projects);
+      if (this.pendencias.length == 0) {
+        this.mostraMsg("Você não possui pendências neste momento.");
+      }
+      console.log(this.pendencias);
+    },
+    mostraErro(texto, segundos) {
+      this.erro = texto;
+      if (texto) {
+        if (segundos) {
+          setTimeout(() => {
+            this.erro = null;
+          }, segundos * 1000);
+        }
+      }
+    },
+    mostraMsg(texto, segundos) {
+      this.msg = texto;
+      if (texto) {
+        if (segundos) {
+          setTimeout(() => {
+            this.msg = null;
+          }, segundos * 1000);
+        }
+      }
+    },
+    maskData(data) {
+      return data.substring(8,10) + "." + data.substring(5,7) + "." + data.substring(0,4);
+    },
+    btnPontuar(aluno){
+      this.mostraMsg("Atalho para pontuação não resolvido para esta Sprint! Nome do Aluno: " + aluno.evaluated.name,3);
+      //alert("Nome do Aluno " + aluno.evaluated.name);
     },
   },
   beforeMount() {
@@ -116,29 +182,29 @@ export default {
   flex-grow: 1;
 }
 
-th, tr, td {
+th,
+tr,
+td {
   border: 0px;
 }
 
 th {
-    text-align: left;
-    padding-left: 5px;
+  text-align: left;
+  padding-left: 5px;
 }
 
 td.pontuar-links {
-    font-weight: bold;
-    float: center;
-    text-align: center;
-    color: rgb(255, 255, 255);
-    background-color: rgb(21, 20, 94);
-    border-radius: 10px 4px 10px 4px;
+  font-weight: bold;
+  float: center;
+  text-align: center;
+  color: rgb(255, 255, 255);
+  background-color: rgb(21, 20, 94);
+  border-radius: 4px 10px 4px 10px;
 }
 
 td.pontuar-links:hover {
-    color: rgb(255, 255, 255);
-    background-color: #164809;
-    border-radius: 4px 10px 4px 10px;
+  color: rgb(255, 255, 255);
+  background-color: #164809;
+  border-radius: 10px 4px 10px 4px;
 }
-
-
 </style>
