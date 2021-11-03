67ec48c3bb5dc090b5073c5ef17b64cc3c146aa9
Raian Damaceno <36999338+RaianDamaceno@users.noreply.github.com>
2021-11-03 19:59:27 -0300

    Login create

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,252 +1,203 @@
 <template>
-    <v-container fluid class='body' fill-height>
-        <v-row justify='center' align='center'>
+  <div class="login">
+    <div class="login-display">
+      <div class="login-display-form" v-if="!telaCadastro">
+        <div>
+          <img src='../../public/img/fatecsj.png' alt="FatecLogo" width="200" height="100">
+        </div>
+        <div>
+          <input class="form_field" placeholder="Usuario, E-mail ou CPF" v-model="login.login"/>
+          <input class="form_field" placeholder="Senha" v-model="login.password"/>
+        </div>
+        <div>
+          <button @click="createLogin"> LogIn </button>
+          <button @click="telaCadastro = true"> Registre-se </button>
+        </div>
+      </div>
 
-            <v-col cols='4' class='inputs' v-if='login.login'>
+      <div class="login-display-form" v-if="telaCadastro">
+         <h3> Quem Sou eu </h3>
+        <div class="login-role-avatares">
+          <div>
+            <v-avatar
+              size="100"
+            >
+            <img
+              @click="cadastro.role='A'"
+              src='../../public/img/administracao.png'
+              alt="John"
+            >
+            </v-avatar>
+         </div>
+          <div>
+            <v-avatar
+              size="100"
+            > 
+            <img
+              src='../../public/img/professores.png'
+              alt="John"
+              @click="cadastro.role='P'"
+            >
+            </v-avatar>
+            </div>
+          <div>    
+            <v-avatar
+              size="100"
+            >
+            <img
+              src='../../public/img/aluna.png'
+              @click="cadastro.role='E'"
+              alt="John"
+            >
+            </v-avatar>
+          </div>
+        </div>
+        <div>
+          <input class="form_field" placeholder="Usuario" v-model="cadastro.login"/>
+          <input class="form_field" placeholder="Nome" v-model="cadastro.name"/>
+          <input class="form_field" placeholder="CPF" v-model="cadastro.document"/>
+          <input class="form_field" placeholder="E-mail" v-model="cadastro.email"/>
+          <input class="form_field" placeholder="Senha" v-model="cadastro.password"/>
+        </div>
+        <div>
+          <button @click="createRegister"> Enviar Cadastro </button>
+        </div>
+      </div>
 
-                <v-row justify='center' align='center'>
-                    <v-img
-                        class="mt-16"
-                        src="../assets/login.png"
-                        max-height="150"
-                        max-width="150"
-                    ></v-img>
-                </v-row>
-
-                <v-col cols='12'>
-                    <v-text-field
-                        v-model='login.firstname'
-                        label='Nome'
-                        required
-                    ></v-text-field>
-                </v-col>
-
-                <v-col cols='12'>
-                     <v-text-field
-                        v-model='login.ra'
-                        label='RA'
-                        required
-                    ></v-text-field>
-                </v-col>
-
-                <v-row justify='center' align='center'>
-                    <v-btn
-                        color='green'
-                        elevation='2'
-                        class='mr-6'>
-                        Entrar
-                    </v-btn>
-
-                    <v-btn
-                        color='primary'
-                        elevation='2'
-                        class='mr-6'
-                        v-on:click='login.signUp = true, login.login = false'>
-                        Cadastrar
-                    </v-btn>
-                </v-row>
-            </v-col>
-
-            <v-col cols='4' class='inputs' v-if='login.signUp'>
-                <v-col cols='12'>
-                    <v-text-field
-                        v-model='signUp.nome'
-                        label='Nome De Usuario'
-                        required
-                    ></v-text-field>
-                </v-col>
-
-                <v-col cols='12'>
-                    <v-text-field
-                        v-model='signUp.ra'
-                        label='Nome'
-                        required
-                    ></v-text-field>
-                </v-col>
-                <v-col cols='12'>
-                    <v-text-field
-                        v-model='signUp.document'
-                        :mask="['###.###.###-##', '##.###.###/####-##']"
-                        label='CPF'
-                        required
-                    ></v-text-field>
-                </v-col>
-                <v-col cols='12'>
-                    <v-text-field
-                        v-model='signUp.email'
-                        label='E-mail Institucional'
-                        required
-                    ></v-text-field>
-                </v-col>
-              <v-col
-                cols="12"
-              >
-              <v-select
-                  v-model="selectRole"
-                  :value="roles.roleName"
-                  :items="roles"
-                  label="Função"
-                  item-text="roleName"
-                  persistent-hint
-                  return-object
-                  single-line
-                ></v-select>
-              </v-col>
-
-                <v-row justify='center' align='center'>
-                    <v-btn
-                        color='green'
-                        elevation='2'
-                        class='mr-6'
-                        v-on:click='registration'>
-                        Registrar
-                    </v-btn>
-
-                    <v-btn
-                        color='red'
-                        elevation='2'
-                        class='mr-6'
-                        v-on:click='login.signUp = false, login.login = true'>
-                        Voltar
-                    </v-btn>
-                </v-row>
-            </v-col>
-
-        </v-row>
-        <v-snackbar v-model='snackbar' :timeout='timeout'>
-            {{ text }}
-            <template v-slot:action='{ attrs }'>
-                <v-btn
-                    :color='colorBtn'
-                    text
-                    v-bind='attrs'
-                    @click='snackbar = false'
-                >
-                    Fechar
-                </v-btn>
-            </template>
-        </v-snackbar>
-    </v-container>
+      <div class="login-display-form-carrousel">
+            <v-carousel height="100%">
+                <v-carousel-item
+                v-for="(item,i) in items"
+                :key="i"
+                :src="item.src"
+                reverse-transition="fade-transition"
+                transition="fade-transition"
+                ></v-carousel-item>
+            </v-carousel>
+      </div>
+    </div>
+  </div>
 </template>
-<style>
-    .body
-    {
-        background-image: url('https://www.drumangle.com/sandbox/wp/21813/wp-content/uploads/2014/01/Login-Screen-Background-Wood-4.jpg');
-    }
-    .inputs
-    {
-        background: white;
-        padding: 0 50px 30px;
-        border-radius: 5px;
-    }
-</style>
-<script lang='ts'>
-import router from '@/router';
-import Vue from 'vue';
-import api from '../services/api'
 
-export default Vue.extend({
-    data: () => ({
-        login:{
-            show1: false,
-            login: true,
-            signUp: false,
-            firstname: '',
-            ra: ''
+<script>
+  import api from '../services/api'
+  export default {
+    name: 'Home',
+     data: () => ({
+       login: {
+         login: '',
+         password: ''
+       },
+      cadastro: {
+          login: '',
+          name: '',
+          document: '',
+          email: '',
+          password: '',
+          role: ''
+      },
+      model: 0,
+      telaCadastro:false,
+      items: [ {
+          src: 'http://blog.feedbackmanager.com.br/wp-content/uploads/2017/08/aprimore.jpg'
         },
-        signUp:{
-            nome: '',
-            email: '',
-            document: '',
-            ra: ''
-        },
-        selectRole: Object,
-        roles: [],
-        snackbar: false,
-        text: '',
-        colorBtn: 'green',
-        timeout: 2000,
+        {
+          src: 'https://c.pxhere.com/images/de/c3/f9e9974b176510264173ebd4a8fc-1584093.jpg!d'
+        }       
+      ],
     }),
-    beforeMount() {
-        api.get('/role').then(response => {
-            this.roles = response.data
-        })
-    },
     methods: {
-        // validate: function(): void
-        // {
-        //     if(this.login.firstname.trim() === '' || this.login.ra.trim() === '')
-        //     {
-        //         this.snackbarShow('Ambos campos são obrigatórios', 'red');
-        //         return;
-        //     }
-
-        //     else
-        //     {
-        //         if(this.login.firstname.trim().toString() === 'adm' && this.login.ra.trim().toString() === '12345678')
-        //         {
-        //             this.snackbarShow('Login realizado com sucesso', 'blue');
-        //             setTimeout(() =>
-        //             {
-        //                 router.push('/dashboard');
-        //             }, 1200)
-        //         }
-        //         else
-        //         {
-        //             this.snackbarShow('Falha no login', 'red');
-        //             return;
-        //         }
-        //     }
-
-        // },
-        // validateRegister: function(): void
-        // {
-        //     if(
-        //         this.signUp.nome.trim() === '' ||
-        //         this.signUp.ra.trim() === '' ||
-        //         this.signUp.email.trim() === '' ||
-        //         this.signUp.nome.trim().length < 2 ||
-        //         this.signUp.ra.trim().length < 6 ||
-        //         this.signUp.email.trim().length < 7
-        //     )
-        //     {
-        //         this.snackbarShow('Todos campos são obrigatórios e Validos, por favor preenche-los Corretamente', 'red');
-        //         return;
-        //     }
-
-        //     this.registration();
-        // },
-        snackbarShow: function(text: string, color = 'green'): void {
-            this.text = text;
-            this.colorBtn = color;
-            this.snackbar = true;
+        goToDashboard(){
+            this.$router.push('/dashboard'); 
         },
-        registration: function(): void {
-            let usuarioRegistro = {
-                    login: this.signUp.ra,
-                    name: this.signUp.nome,
-                    document: this.signUp.document,
-                    email: this.signUp.email,
-                    role: this.selectRole.roleName
-            }   
-
-            api.post("user", usuarioRegistro).then(response =>{
-                 if(response.status === 201) {
-                        this.snackbarShow(
-                            'Cadastro Realizado com sucesso, você será direcionado ao DashBoard'
-                        );
-                        setTimeout(() => {
-                            router.push('/dashboard');
-                        }, 2200);
-                    }
-                })
-                .catch(() => {
-                    this.snackbarShow(
-                        'Ocorreu um erro ao realizar a operação :( por favor repita daqui a alguns instantes',
-                        'red'
-                    );
-                })
+        createRegister() {
+            api.post('/user', this.cadastro).then(response =>{
+              response.status == 201 ? this.$router.push('/login'):alert("Erro ao realizar cadastro"); 
+            })
+        },
+        createLogin() {
+          api.post('/auth/login', this.login).then(response => {
+            response.status == 201 ? this.$router.push('/dashboard') :alert("Erro ao realizar Login"); 
+          })
         }
     }
-});
+  }
 </script>
+
+<style scoped lang="scss">
+  .login {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url('../../public/img/bg_login.jpg');
+
+  }
+
+  .login-display {
+    height: 65%;
+    width: 70%;
+    border: solid 1px #DCDCDC;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .login-display-form {
+    height: 100%;
+    width: 35%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  .login-display-form-carrousel {
+    height: 100%;
+    width: 65%;
+  }
+
+  button {
+    width: 330px;
+    height: 45px;
+    border: solid 1px black;
+    border-radius: 30px;
+    margin-top: 10px;
+  }
+
+  .form_field {
+    font-family: inherit;
+    width: 330px;
+    border: 0;
+    border-bottom: 2px solid gray;
+    font-size: 1.3rem;
+    color: black;
+    padding: 7px 0;
+    background: transparent;
+    transition: border-color 0.2s;
+    margin-top: 10px;
+  }
+
+  .login-display-form div {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    align-content: space-between;
+  }
+
+  .login-role-avatares {
+    display: flex;
+    justify-content: space-between !important;
+    flex-direction: row !important;
+  }
+
+  .login-role-avatares div {
+    padding: 10px;
+  }
+
+  .login-role-avatares img {
+    -webkit-box-shadow: 3px 27px 34px 5px rgba(0,0,0,0.53); 
+    box-shadow: 3px 27px 34px 5px rgba(0,0,0,0.53);
+  }
+</style>
+
