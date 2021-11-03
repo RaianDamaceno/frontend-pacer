8fcb3c8549d9275e225926e929f623ce41208b0b
paullo97 <paulo.cesar97@hotmail.com>
2021-11-03 11:10:14 -0300

    include data of graph with real data

--- src/components/GraphSpider.vue
+++ src/components/GraphSpider.vue
@@ -3,13 +3,16 @@
 </template>
 
 <style>
-.hc {
-    height: 150%;
-}
+    .hc
+    {
+        height: 150%;
+    }
 </style>
 
 <script>
 import Vue from "vue";
+Vue.prototype.total = [];
+
 export default Vue.extend({
     name: "GraphSpider",
     props: {
@@ -27,44 +30,26 @@ export default Vue.extend({
         },
     },
     beforeMount() {
-        // this.notas
-        //     .filter((data) => data.idEvaluated === '22')
-        //     .map((data) => {
-        //         console.log(data.note);
-        //     });
-        // this.criterios.map((cri) => {
-        //     this.notas.map((nota) => {
-        //         if (cri.idCriteria === nota.criterio.idCriteria) {
-        //               this.notasGraph.push(
-        //                 nota.note === null
-        //                 ? 0
-        //                 : nota.note
-        //             );
-        //         }
-        //     });
-        // });
         this.notas.map((not) => {
-            this.criterios.map((cri) => {
-                if (cri.idCriteria === not.criterio.idCriteria) {
+            this.criterios
+                .filter((cr) => cr.idCriteria === not.criterio.idCriteria)
+                .map((crit) => {
                     this.notasGraph.push({
-                        nome: cri.descCriteria,
+                        nome: crit.descCriteria,
                         nota: not.note === null ? 0 : not.note,
                     });
-                }
-            });
+                });
         });
 
-        console.log("Notas do Grafico - ", this.notasGraph);
-
-        for (var i = 0; i < this.notasGraph.length - 1; i++) {
-            for (var n = 0; n < this.notasGraph.length - i - 1; n++) {
-                if (this.notasGraph[n].nome === this.notasGraph[n+1].nome) {
-                    // console.log('teste');
-                    console.log(this.notasGraph[n].nome);
-                    console.log(this.notasGraph[n].nota);
-                }
-            }
-        }
+        this.criterios.map((crit) => {
+            this.total.push(
+                this.notasGraph
+                    .filter((nt) => nt.nome === crit.descCriteria)
+                    .reduce(function(total, numero) {
+                        return parseInt(total + numero.nota);
+                    }, 0)
+            );
+        });
     },
     data() {
         return {
@@ -112,22 +97,7 @@ export default Vue.extend({
                 series: [
                     {
                         name: "Sprint 1",
-                        data: [0, 4, 6, 6, 7],
-                        pointPlacement: "on",
-                    },
-                    {
-                        name: "Sprint 2",
-                        data: [2, 3, 6, 8, 7],
-                        pointPlacement: "on",
-                    },
-                    {
-                        name: "Sprint 3",
-                        data: [1, 4, 6, 5, 2],
-                        pointPlacement: "on",
-                    },
-                    {
-                        name: "Sprint 4",
-                        data: [3, 4, 6, 3, 7],
+                        data: this.total,
                         pointPlacement: "on",
                     },
                 ],
