c836196cd15e7aaf8b3b03325e39feac02f8ecf3
paullo97 <paulo.cesar97@hotmail.com>
2021-11-06 00:58:00 -0300

    work in progress

--- src/components/GraphSpider.vue
+++ src/components/GraphSpider.vue
@@ -39,7 +39,6 @@ export default Vue.extend({
     },
     data() {
         return {
-            notasGraph: [],
             chartOptions: {
                 chart: {
                     type: "line",
