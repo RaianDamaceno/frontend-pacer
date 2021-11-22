da06fdd8692a2927d29a776f24af1c969c26a339
MarcosVP <marcos.pereira47@fatec.sp.gov.br>
2021-11-22 02:47:30 -0300

    Criteria - Refatoração

--- src/components/CriteriaRegistration.vue
+++ src/components/CriteriaRegistration.vue
@@ -1,82 +1,103 @@
 <template>
-  <v-dialog v-model="dialog" scrollable max-width="400px" justify="center">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn class="black--text" color="white" dark v-bind="attrs" v-on="on"
-        >Lista de Critérios
-      </v-btn>
-    </template>
-    <v-card>
-      <v-card-title v-if="this.id === ''">Cadastrar Novo Critério de Avaliação</v-card-title>
-      <v-card-title v-else               >Alterar Critério de Avaliação</v-card-title>
-      <v-divider></v-divider>
+  <v-row justify="left">
+    <v-dialog v-model="dialog" width="100%">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn 
+          v-bind="attrs"
+            v-on="on"
+            width="100%"
+            class="light-blue darken-3"
+            elevation="0"
+            x-large> Lista de Critérios
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title v-if="this.id === ''">
+          <span class="text-h5">Cadastrar Novo Critério de Avaliação</span>
+        </v-card-title
+        >
+        <v-card-title v-else>
+          <span class="text-h5">Alterar Critério de Avaliação</span>
+        </v-card-title>
+        <v-divider></v-divider>
 
-      <v-card-text>
+        <v-card-text>
+          <div class="d-flex justify-left" id="descCriteria">
+            <v-text-field
+              label="Descrição *"
+              v-model="descriptionInput"
+              hint="Descrição breve do critério"
+              counter="30"
+              @keyup.native.enter="validate"
+            >
+            </v-text-field>
+          </div>
 
-        <div class="d-flex justify-left" id="descCriteria">
-          <v-text-field label="Descrição" 
-                        v-model="descriptionInput" 
-                        hint="Descrição breve do critério" 
-                        counter="30" 
-                        @keyup.native.enter="validate">
-          </v-text-field>
-        </div>
+          <div class="d-flex justify-left" id="obs">
+            <v-textarea
+              label="Explicação"
+              v-model="obs"
+              auto-grow
+              dense
+              clearable
+              hint="Explicação sobre a que este critério se refere"
+              counter="500"
+              row-height="6"
+            >
+            </v-textarea>
+          </div>
 
-        <div class="d-flex justify-left" id="obs">
-          <v-textarea label="Explicação" v-model="obs"
-           auto-grow 
-           dense 
-           clearable
-           hint="Explicação sobre a que este critério se refere" 
-           counter="500" 
-           row-height="6">
-          </v-textarea>
-        </div>
+          <div v-if="this.id !== ''" class="d-flex justify-left">
+            <v-checkbox
+              label="Inativo"
+              color="primary"
+              v-model="inactive"
+              dense
+            >
+            </v-checkbox>
+          </div>
+          <small>* Campos obrigatórios</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="close()">Fechar</v-btn>
+          <v-btn color="blue darken-1" text v-on:click="validate">Salvar</v-btn>
+        </v-card-actions>
 
-        <div v-if="this.id !== ''" class="d-flex justify-left">
-          <v-checkbox label="Inativo" color="primary" v-model="inactive" dense>
-          </v-checkbox>
-        </div>
+        <v-divider></v-divider>
 
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text v-on:click="validate">Salvar</v-btn>
-        <v-btn color="blue darken-1" text @click="close()">Fechar</v-btn>
-      </v-card-actions>
-
-      <v-divider></v-divider>
-
-      <v-card-text>
-        <v-container>
-          <table v-if="this.listCriteria.length > 0">
-            <thead>
-              <th width="200px" style="text-align: center">Critério</th>
-              <th width="80px" style="text-align: center">Situação</th>
-              <th width="120px" style="text-align: center"></th>
-            </thead>
-            <tbody>
-              <tr v-for="crit in listCriteria" :key="crit.descCriteria">
-                <td style="text-align: center">{{ crit.descCriteria }}</td>
-                <td v-if="crit.snAtivo == 'S'" style="text-align: center">
-                  ativo
-                </td>
-                <td v-else style="text-align: center; color: red">inativo</td>
-                <td>
-                  <button @click="editCriteria(crit)" class="btn_editar">
-                    <i>editar</i>
-                  </button>
-                  <span> </span>
-                  <button @click="deleteCriteria(crit)" class="btn_excluir">
-                    <i>&nbsp;excluir</i>
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </v-container>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+        <v-card-text>
+          <v-container>
+            <table v-if="this.listCriteria.length > 0">
+              <thead>
+                <th width="200px" style="text-align: center">Critério</th>
+                <th width="80px" style="text-align: center">Situação</th>
+                <th width="120px" style="text-align: center"></th>
+              </thead>
+              <tbody>
+                <tr v-for="crit in listCriteria" :key="crit.descCriteria">
+                  <td style="text-align: center">{{ crit.descCriteria }}</td>
+                  <td v-if="crit.snAtivo == 'S'" style="text-align: center">
+                    ativo
+                  </td>
+                  <td v-else style="text-align: center; color: red">inativo</td>
+                  <td>
+                    <button @click="editCriteria(crit)" class="btn_editar">
+                      <i>editar</i>
+                    </button>
+                    <span> </span>
+                    <button @click="deleteCriteria(crit)" class="btn_excluir">
+                      <i>&nbsp;excluir</i>
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </v-row>
 </template>
 <script lang="ts">
 import api from "@/services/api";
@@ -121,13 +142,15 @@ export default Vue.extend({
             document.getElementById("descCriteria")?.focus();
           })
           .catch((error) => {
-            this.$store.dispatch("messageError",error.response.data.message);
+            this.$store.dispatch("messageError", error.response.data.message);
           });
       } else {
         api
-          .put("criteria/" + this.id, {"descCriteria": this.descriptionInput,
-                                       "obs": this.obs,
-                                       "snAtivo": this.inactive ? "N" : "S"} )
+          .put("criteria/" + this.id, {
+            descCriteria: this.descriptionInput,
+            obs: this.obs,
+            snAtivo: this.inactive ? "N" : "S",
+          })
           .then(() => {
             this.fetchCriterias();
             this.resetValues();
@@ -138,7 +161,7 @@ export default Vue.extend({
             document.getElementById("descCriteria")?.focus();
           })
           .catch((error) => {
-            this.$store.dispatch("messageError",error.response.data.message);
+            this.$store.dispatch("messageError", error.response.data.message);
           });
       }
     },
@@ -192,14 +215,6 @@ export default Vue.extend({
   },
   beforeMount() {
     this.fetchCriterias();
-  },
-  mounted() {
-    // let self = this;
-    // window.addEventListener("keyup", function (event) {
-    //   if (event.keyCode === 13) {
-    //     self.saveCriteria();
-    //   }
-    // });
   },
 });
 </script>
